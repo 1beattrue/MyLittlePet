4c713bf2125f44d1d38ae599752ebc16c2d55a7e
Vitaly <onebeattrue@gmail.com>
2024-02-12 01:18:38 +0400

    Сделал ошибки дата классами

--- app/src/main/java/edu/mirea/onebeattrue/mylittlepet/domain/auth/entity/AuthExceptions.kt
+++ app/src/main/java/edu/mirea/onebeattrue/mylittlepet/domain/auth/entity/AuthExceptions.kt
@@ -1,42 +1,56 @@
 package edu.mirea.onebeattrue.mylittlepet.domain.auth.entity
 
-class InvalidPhoneNumberException(message: String = TEXT_MESSAGE) : Exception(message) {
+data class InvalidPhoneNumberException(
+    override val message: String = TEXT_MESSAGE
+) : Exception(message) {
     companion object {
         private const val TEXT_MESSAGE = "Invalid phone number"
     }
 }
 
-class InvalidVerificationCodeException(message: String = TEXT_MESSAGE) : Exception(message) {
+data class InvalidVerificationCodeException(
+    override val message: String = TEXT_MESSAGE
+) : Exception(message) {
     companion object {
         private const val TEXT_MESSAGE = "Invalid verification code"
     }
 }
 
-class TimeoutVerificationCodeException(message: String = TEXT_MESSAGE) : Exception(message) {
+data class TimeoutVerificationCodeException(
+    override val message: String = TEXT_MESSAGE
+) : Exception(message) {
     companion object {
         private const val TEXT_MESSAGE = "Timeout has not expired"
     }
 }
 
-class UnknownAuthException(message: String = TEXT_MESSAGE) : Exception(message) {
+data class UnknownAuthException(
+    override val message: String = TEXT_MESSAGE
+) : Exception(message) {
     companion object {
         private const val TEXT_MESSAGE = "Unknown exception"
     }
 }
 
-class NetworkAuthException(message: String = TEXT_MESSAGE) : Exception(message) {
+data class NetworkAuthException(
+    override val message: String = TEXT_MESSAGE
+) : Exception(message) {
     companion object {
         private const val TEXT_MESSAGE = "Network exception"
     }
 }
 
-class InvalidCredentialsAuthException(message: String = TEXT_MESSAGE) : Exception(message) {
+data class InvalidCredentialsAuthException(
+    override val message: String = TEXT_MESSAGE
+) : Exception(message) {
     companion object {
         private const val TEXT_MESSAGE = "Invalid confirmation code"
     }
 }
 
-class TooManyRequestsAuthException(message: String = TEXT_MESSAGE) : Exception(message) {
+data class TooManyRequestsAuthException(
+    override val message: String = TEXT_MESSAGE
+) : Exception(message) {
     companion object {
         private const val TEXT_MESSAGE = "Too many requests"
     }
