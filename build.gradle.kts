8d7439947da577d682948e09096d1871d84c145f
Vitaly <onebeattrue@gmail.com>
2024-05-02 21:30:44 +0300

    Удалил легаси

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.google.gms.google-services") version "4.4.1" apply false
-    id("com.android.application") version "8.3.2" apply false
+    id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
     kotlin("plugin.serialization") version "1.9.0"
