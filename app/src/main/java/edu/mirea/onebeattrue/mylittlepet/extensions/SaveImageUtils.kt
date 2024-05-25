08f8e7aecd8fa8a0e5b5f521051262d835b91be9
Vitaly <onebeattrue@gmail.com>
2024-05-25 16:05:49 +0300

    Пофиксил вылет из-за пустого uri

--- app/src/main/java/edu/mirea/onebeattrue/mylittlepet/extensions/SaveImageUtils.kt
+++ app/src/main/java/edu/mirea/onebeattrue/mylittlepet/extensions/SaveImageUtils.kt
@@ -7,15 +7,18 @@ import java.io.File
 import java.io.FileOutputStream
 
 fun saveImageToInternalStorage(context: Context, uri: Uri): Uri {
-    val bytes = context.contentResolver.openInputStream(uri)?.use { inputStream ->
-        inputStream.readBytes()
-    }
-    val fileName = "${System.currentTimeMillis()}.jpg"
-    val file = File(context.filesDir, fileName)
+    if (uri != Uri.EMPTY) {
+        val bytes = context.contentResolver.openInputStream(uri)?.use { inputStream ->
+            inputStream.readBytes()
+        }
+        val fileName = "${System.currentTimeMillis()}.jpg"
+        val file = File(context.filesDir, fileName)
 
-    FileOutputStream(file).use { outputStream ->
-        outputStream.write(bytes)
-    }
+        FileOutputStream(file).use { outputStream ->
+            outputStream.write(bytes)
+        }
 
-    return file.toUri()
+        return file.toUri()
+    }
+    return Uri.EMPTY
 }
