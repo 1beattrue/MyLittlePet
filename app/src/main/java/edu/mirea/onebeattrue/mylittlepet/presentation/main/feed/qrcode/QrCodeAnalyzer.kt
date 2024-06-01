e7828e8451d250646d6150a524df6e2f57513ff3
Vitaly <onebeattrue@gmail.com>
2024-06-02 01:43:01 +0300

    Обновил класс QrCodeAnalyzer Стало работать намного быстрее и лучше

--- app/src/main/java/edu/mirea/onebeattrue/mylittlepet/presentation/main/feed/qrcode/QrCodeAnalyzer.kt
+++ app/src/main/java/edu/mirea/onebeattrue/mylittlepet/presentation/main/feed/qrcode/QrCodeAnalyzer.kt
@@ -23,35 +23,39 @@ class QrCodeAnalyzer(
 
     override fun analyze(image: ImageProxy) {
         if (image.format in supportedImageFormats) {
-            val bytes = image.planes.first().buffer.toByteArray()
+            if (image.planes.size == 3) {
+                val rotatedImage =
+                    RotatedImage(getLuminancePlaneData(image), image.width, image.height)
+                rotateImageArray(rotatedImage, image.imageInfo.rotationDegrees)
 
-            val source = PlanarYUVLuminanceSource(
-                bytes,
-                image.width,
-                image.height,
-                0,
-                0,
-                image.width,
-                image.height,
-                false
-            )
+                val source = PlanarYUVLuminanceSource(
+                    rotatedImage.byteArray,
+                    rotatedImage.width,
+                    rotatedImage.height,
+                    0,
+                    0,
+                    rotatedImage.width,
+                    rotatedImage.height,
+                    false
+                )
 
-            val binaryBitmap = BinaryBitmap(HybridBinarizer(source))
-            try {
-                val result = MultiFormatReader().apply {
-                    setHints(
-                        mapOf(
-                            DecodeHintType.POSSIBLE_FORMATS to arrayListOf(
-                                BarcodeFormat.QR_CODE
+                val binaryBitmap = BinaryBitmap(HybridBinarizer(source))
+                try {
+                    val result = MultiFormatReader().apply {
+                        setHints(
+                            mapOf(
+                                DecodeHintType.POSSIBLE_FORMATS to arrayListOf(
+                                    BarcodeFormat.QR_CODE
+                                )
                             )
                         )
-                    )
-                }.decode(binaryBitmap)
-                onQrCodeScanned(result.text)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            } finally {
-                image.close()
+                    }.decode(binaryBitmap)
+                    onQrCodeScanned(result.text)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    image.close()
+                }
             }
         }
     }
@@ -62,4 +66,57 @@ class QrCodeAnalyzer(
             get(it)
         }
     }
+
+    private fun getLuminancePlaneData(image: ImageProxy): ByteArray {
+        val plane = image.planes.first()
+        val buf: ByteBuffer = plane.buffer
+        val data = ByteArray(buf.remaining())
+        buf.get(data)
+        buf.rewind()
+        val width = image.width
+        val height = image.height
+        val rowStride = plane.rowStride
+        val pixelStride = plane.pixelStride
+
+        val cleanData = ByteArray(width * height)
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                cleanData[y * width + x] = data[y * rowStride + x * pixelStride]
+            }
+        }
+        return cleanData
+    }
+
+    private fun rotateImageArray(imageToRotate: RotatedImage, rotationDegrees: Int) {
+        if (rotationDegrees == 0) return
+        if (rotationDegrees % 90 != 0) return
+
+        val width = imageToRotate.width
+        val height = imageToRotate.height
+
+        val rotatedData = ByteArray(imageToRotate.byteArray.size)
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                when (rotationDegrees) {
+                    90 -> rotatedData[x * height + height - y - 1] =
+                        imageToRotate.byteArray[x + y * width]
+
+                    180 -> rotatedData[width * (height - y - 1) + width - x - 1] =
+                        imageToRotate.byteArray[x + y * width]
+
+                    270 -> rotatedData[y + x * height] =
+                        imageToRotate.byteArray[y * width + width - x - 1]
+                }
+            }
+        }
+
+        imageToRotate.byteArray = rotatedData
+
+        if (rotationDegrees != 180) {
+            imageToRotate.height = width
+            imageToRotate.width = height
+        }
+    }
 }
+
+private class RotatedImage(var byteArray: ByteArray, var width: Int, var height: Int)
