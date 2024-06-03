d748b24e171bd4b6bbea99208cb495c634f22d6b
Vitaly <onebeattrue@gmail.com>
2024-06-03 19:11:36 +0300

    Запрос разрешения на использование камеры

--- app/src/main/java/edu/mirea/onebeattrue/mylittlepet/presentation/main/feed/qrcode/QrCodeContent.kt
+++ app/src/main/java/edu/mirea/onebeattrue/mylittlepet/presentation/main/feed/qrcode/QrCodeContent.kt
@@ -1,5 +1,6 @@
 package edu.mirea.onebeattrue.mylittlepet.presentation.main.feed.qrcode
 
+import android.Manifest
 import android.util.Size
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
@@ -7,10 +8,16 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.CameraAlt
+import androidx.compose.material.icons.rounded.Error
+import androidx.compose.material.icons.rounded.Videocam
+import androidx.compose.material.icons.rounded.VideocamOff
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,6 +28,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,12 +38,20 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
+import com.google.common.util.concurrent.ListenableFuture
 import edu.mirea.onebeattrue.mylittlepet.R
+import edu.mirea.onebeattrue.mylittlepet.ui.customview.CustomCardExtremeElevation
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
 fun QrCodeContent(
     modifier: Modifier = Modifier,
@@ -47,6 +63,12 @@ fun QrCodeContent(
 
     val cameraProviderFuture = remember {
         ProcessCameraProvider.getInstance(context)
+    }
+
+    val permissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
+
+    LaunchedEffect(key1 = Unit) {
+        permissionState.launchPermissionRequest()
     }
 
     DisposableEffect(Unit) {
@@ -87,54 +109,87 @@ fun QrCodeContent(
                 .padding(paddingValues)
                 .fillMaxSize()
         ) {
-            AndroidView(
-                modifier = Modifier.fillMaxSize(),
-                factory = { context ->
-                    val previewView = PreviewView(context)
-                    val preview = Preview.Builder()
-                        .build()
-                    val selector = CameraSelector.Builder()
-                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                        .build()
-                    preview.setSurfaceProvider(previewView.surfaceProvider)
-                    val imageAnalysis = ImageAnalysis.Builder()
-                        .setTargetResolution(
-                            Size(
-                                previewView.width,
-                                previewView.height
-                            )
-                        )
-                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                        .build()
-                    imageAnalysis.setAnalyzer(
-                        ContextCompat.getMainExecutor(context),
-                        QrCodeAnalyzer { result ->
-                            component.onCodeScanned(result)
-                        }
-                    )
-                    try {
-                        cameraProviderFuture.get().bindToLifecycle(
-                            lifecycleOwner,
-                            selector,
-                            preview,
-                            imageAnalysis
-                        )
-                    } catch (e: Exception) {
-                        e.printStackTrace()
-                    }
-                    previewView
+            if (permissionState.status.isGranted) {
+                QrCodeScanner(
+                    onCodeScanned = { component.onCodeScanned(it) },
+                    cameraProviderFuture = cameraProviderFuture,
+                    lifecycleOwner = lifecycleOwner
+                )
+            } else {
+                val (icon, text) = if (permissionState.status.shouldShowRationale) {
+                    Icons.Rounded.Videocam to stringResource(R.string.camera_permission_required)
+                } else {
+                    Icons.Rounded.VideocamOff to stringResource(R.string.camera_permission_denied)
                 }
-            )
-            Icon(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .align(Alignment.Center)
-                    .padding(16.dp)
-                    .alpha(0.5f),
-                painter = painterResource(R.drawable.scanner_border),
-                contentDescription = null,
-                tint = Color.Gray,
-            )
+
+                CustomCardExtremeElevation(
+                    modifier = Modifier.align(Alignment.Center)
+                ) {
+                    Icon(imageVector = icon, contentDescription = null)
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = text,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
         }
     }
+}
+
+@Composable
+private fun BoxScope.QrCodeScanner(
+    modifier: Modifier = Modifier,
+    onCodeScanned: (String) -> Unit,
+    cameraProviderFuture: ListenableFuture<ProcessCameraProvider>,
+    lifecycleOwner: LifecycleOwner
+) {
+    AndroidView(
+        modifier = modifier.fillMaxSize(),
+        factory = { context ->
+            val previewView = PreviewView(context)
+            val preview = Preview.Builder()
+                .build()
+            val selector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+            preview.setSurfaceProvider(previewView.surfaceProvider)
+            val imageAnalysis = ImageAnalysis.Builder()
+                .setTargetResolution(
+                    Size(
+                        previewView.width,
+                        previewView.height
+                    )
+                )
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+            imageAnalysis.setAnalyzer(
+                ContextCompat.getMainExecutor(context),
+                QrCodeAnalyzer { result ->
+                    onCodeScanned(result)
+                }
+            )
+            try {
+                cameraProviderFuture.get().bindToLifecycle(
+                    lifecycleOwner,
+                    selector,
+                    preview,
+                    imageAnalysis
+                )
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            previewView
+        }
+    )
+    Icon(
+        modifier = Modifier
+            .fillMaxSize()
+            .align(Alignment.Center)
+            .padding(16.dp)
+            .alpha(0.5f),
+        painter = painterResource(R.drawable.scanner_border),
+        contentDescription = null,
+        tint = Color.Gray,
+    )
 }
