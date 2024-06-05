3725dd0fe0924f102faf878a424796c4833bd532
Vitaly <onebeattrue@gmail.com>
2024-06-05 13:03:20 +0300

    Добавил отступ после префикса номера телефона

--- app/src/main/java/edu/mirea/onebeattrue/mylittlepet/presentation/auth/phone/PhoneContent.kt
+++ app/src/main/java/edu/mirea/onebeattrue/mylittlepet/presentation/auth/phone/PhoneContent.kt
@@ -4,11 +4,13 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -113,7 +115,10 @@ fun PhoneContent(
                     singleLine = true,
                     keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     prefix = {
-                        Text(stringResource(id = R.string.phone_number_prefix))
+                        Row {
+                            Text(stringResource(id = R.string.phone_number_prefix))
+                            Spacer(modifier = Modifier.width(8.dp))
+                        }
                     },
                     placeholder = {
                         Text(
