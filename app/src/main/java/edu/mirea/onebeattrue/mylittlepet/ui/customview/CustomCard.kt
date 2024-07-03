49b03ae9ad929aa1ad0cf58748d82a26bdafcc01
Vitaly <onebeattrue@gmail.com>
2024-07-03 23:06:11 +0300

    Уже совсем похоже на правду

--- app/src/main/java/edu/mirea/onebeattrue/mylittlepet/ui/customview/CustomCard.kt
+++ app/src/main/java/edu/mirea/onebeattrue/mylittlepet/ui/customview/CustomCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Error
 import androidx.compose.material.icons.rounded.Refresh
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
@@ -23,7 +24,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.matяerial.icons.rounded.Error
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
